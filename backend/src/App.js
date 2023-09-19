5ec431a8a2138fa8c04c6eb1eb300c8923a17889
Brenda <amaya.brendaa@gmail.com>
2023-09-18 19:21:26 -0700

    creating layout for blog posts

--- backend/src/App.js
+++ backend/src/App.js
@@ -1,12 +1,53 @@
-
 import './App.css';
 
 function App() {
   return (
     <main>
       <header>
-        <a href='' className= 'chisme logo'>Chisme</a>
+        <a href='https://assets-jpcust.jwpsrv.com/thumbnails/u6e0g7ki-720.jpg' className= 'chisme logo'>Chisme</a>
+
+        <nav>
+          <a href=''>Log In</a>
+          <a href=''>Register</a>
+        </nav>
+
       </header>
+
+      <div className='post'>
+        <div className='image'>
+          <img src='https://shorturl.at/jpr57' alt='cruel summer image' />
+        </div>
+
+        <div className='text'>
+          <h2>Cruel Summer Now on Hulu!</h2>
+          <p>Season 2 of Cruel Summer is now out on Hulu. </p>
+        </div>
+      </div>
+
+
+      <div className='post'>
+        <div className='image'>
+          <img src='https://shorturl.at/jpr57' alt='cruel summer image' />
+        </div>
+
+        <div className='text'>
+          <h2>Cruel Summer Now on Hulu!</h2>
+          <p>Season 2 of Cruel Summer is now out on Hulu. </p>
+        </div>
+      </div>
+
+
+      <div className='post'>
+        <div className='image'>
+          <img src='https://shorturl.at/jpr57' alt='cruel summer image' />
+        </div>
+
+        <div className='text'>
+          <h2>Cruel Summer Now on Hulu!</h2>
+          <p>Season 2 of Cruel Summer is now out on Hulu. </p>
+        </div>
+      </div>
+
     </main>
   );
 }
