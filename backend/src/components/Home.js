150da95c661d95fe52e15205103fca6cde01d8ee
Brenda <amaya.brendaa@gmail.com>
2023-09-18 19:55:52 -0700

    made own section for posts for cleaner look

--- backend/src/components/Home.js
+++ backend/src/components/Home.js
@@ -1,70 +1,15 @@
 import '../css/Home.css'
+import Post from './Post'
 
 function Home () {
     return (
     <main>
-      <div className='post'>
 
-        <div className='image'>
-          <img src='https://shorturl.at/jpr57' alt='cruel summer image' />
-        </div>
+      <Post />
+      <Post />
+      <Post />
 
-        <div className='text'>
-          <h2>Cruel Summer Now on Hulu!</h2>
-            <p className="info">
-              <a className="author">Beebz</a>
-              <time>2023-09-12 07:15</time>
-            </p>
-
-            <p className="summary">
-            Has anyone watched season two of cruel summer??? its out now!!!!
-            </p>
-        </div>
-
-      </div>
-
-      <div className='post'>
-
-        <div className='image'>
-          <img src='https://shorturl.at/jpr57' alt='cruel summer image' />
-        </div>
-
-        <div className='text'>
-          <h2>Cruel Summer Now on Hulu!</h2>
-            <p className="info">
-              <a className="author">Beebz</a>
-              <time>2023-09-12 07:15</time>
-            </p>
-            
-            <p className="summary">
-            Has anyone watched season two of cruel summer??? its out now!!!!
-            </p>
-        </div>
-
-      </div>
-
-      <div className='post'>
-
-        <div className='image'>
-          <img src='https://shorturl.at/jpr57' alt='cruel summer image' />
-        </div>
-
-        <div className='text'>
-          <h2>Cruel Summer Now on Hulu!</h2>
-            <p className="info">
-              <a className="author">Beebz</a>
-              <time>2023-09-12 07:15</time>
-            </p>
-            
-            <p className="summary">
-            Has anyone watched season two of cruel summer??? its out now!!!!
-            </p>
-        </div>
-
-      </div>
-
-
-      </main>
+    </main>
     )
 }
 
