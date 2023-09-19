f2ea3956329c323504b0c0732b8c81e523a49f93
Brenda <amaya.brendaa@gmail.com>
2023-09-18 19:02:52 -0700

    initializing

---
+++ backend/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href='' className= 'chisme logo'>Chisme</a>
+      </header>
+    </main>
+  );
+}
+
+export default App;
